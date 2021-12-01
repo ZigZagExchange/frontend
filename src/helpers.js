583a9c54dfc7279a3b52e3dea80c110f86fa5adc
taureau <taureau99@protonmail.com>
2021-12-01 15:23:21 -0800

    fix pubkey fee currency

--- src/helpers.js
+++ src/helpers.js
@@ -390,10 +390,16 @@ export async function signinzksync(chainid) {
 export async function changepubkeyzksync() {
     let feeToken = "ETH";
     const balances = accountState.committed.balances;
-    if (!balances.ETH || balances.ETH < 0.005e18) {
+    console.log(balances);
+    console.log(balances.ETH < 0.005e18);
+    if (balances.ETH && balances.ETH > 0.005e18) {
+        feeToken = "ETH";
+    } else if (balances.USDC && balances.USDC > 20e6) {
         feeToken = "USDC";
-    } else if (!balances.USDC || balances.USDC < 20e6) {
+    } else if (!balances.USDC && balances.USDC > 20e6) {
         feeToken = "USDT";
+    } else {
+        feeToken = "ETH";
     }
     const changePubkey = await syncWallet.setSigningKey({
         feeToken,
