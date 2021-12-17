850327a38aef4ab77b3b585780dddc905710a407
taureau <taureau99@protonmail.com>
2021-12-17 19:29:49 +0530

    auto sign in on page load

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,10 +1,19 @@
+import { useSelector } from 'react-redux'
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import TradePage from 'components/pages/TradePage/TradePage'
 import BridgePage from 'components/pages/BridgePage/BridgePage'
 import PoolPage from 'components/pages/PoolPage/PoolPage'
+import { networkSelector } from 'lib/store/features/api/apiSlice'
+import api from 'lib/api'
 
 const AppRoutes = () => {
+  // persist login across sessions
+  const network = useSelector(networkSelector)
+  if (localStorage.getItem("zksync:seed")) {
+    api.signIn(network);
+  }
+
   return (
     <>
       <Router>
