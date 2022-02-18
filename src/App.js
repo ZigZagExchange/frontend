7f20d5bd5cb5434fd9e7feecc857af3b6e965045
Caleb Carithers <calebcarithers@me.com>
2022-02-18 17:54:20 +0000

    sentry

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import Provider from 'lib/Provider'
 import api from 'lib/api'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import 'App.css'
+import * as Sentry from "@sentry/react"
+import {BrowserTracing} from "@sentry/tracing"
+import {isProduction} from "./lib/helpers/env";
 
 api.start()
+
+if (isProduction()) {
+  Sentry.init({
+    dsn: "https://6438a6fc06ae48ac94f3f3046b04199b@o1147437.ingest.sentry.io/6217691",
+    integrations: [new BrowserTracing()],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 class App extends React.Component {
   render() {
--- src/lib/helpers/env.jsx
+++ src/lib/helpers/env.jsx
@@ -1,4 +1,6 @@
 export const isDev = () => process.env.NODE_ENV === 'development'
+export const isProduction = () => process.env.NODE_ENV === 'production'
+
 export const Dev = ({children}) => {
     return isDev() ? children : <></>
 }
