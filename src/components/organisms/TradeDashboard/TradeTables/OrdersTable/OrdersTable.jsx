9d226a058ddac2c26632b6797c30efee310252ea
TrooperCrypto <trooper.crypto7@gmail.com>
2022-02-27 15:58:52 +0100

    fix/show fee as '--' while broadcasting

--- src/components/organisms/TradeDashboard/TradeTables/OrdersTable/OrdersTable.jsx
+++ src/components/organisms/TradeDashboard/TradeTables/OrdersTable/OrdersTable.jsx
@@ -216,6 +216,9 @@ export class OrdersTable extends React.Component {
             const marketInfo = api.marketInfo[market];
             if(feeamount && feetoken) {
               feeText = `${feeamount} ${feetoken}`;
+            } else if(fillstatus == "b") {
+              feeText = "--";
+              // cases below make it backward compatible:
             } else if (!marketInfo) {
               feeText = "1 USDC";
             } else if (fillstatus === "r" || !api.isZksyncChain()) {
