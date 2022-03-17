9bbae285c9922ed98ab4ca55e05cdae7fe807a5e
WonderDev21 <wonder.dev21@gmail.com>
2022-03-18 03:34:35 +0900

    feat: updated time field format

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -98,18 +98,10 @@ export function formatDate(date) {
       padTo2Digits(date.getSeconds()),
     ].join(":");
   } else {
-    return (
-      [
-        padTo2Digits(date.getDate()),
-        padTo2Digits(date.getMonth() + 1),
-        date.getFullYear(),
-      ].join("-") +
-      " " +
-      [
-        padTo2Digits(date.getHours()),
-        padTo2Digits(date.getMinutes()),
-        padTo2Digits(date.getSeconds()),
-      ].join(":")
-    );
+    return [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join("-");
   }
 }
