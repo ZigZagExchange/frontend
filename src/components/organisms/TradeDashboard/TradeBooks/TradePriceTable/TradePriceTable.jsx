856df8e69553d5368df670c047c694ff52fcae63
WonderDev21 <wonder.dev21@gmail.com>
2022-03-23 02:21:38 +0900

    fix: removed unused state value

--- src/components/organisms/TradeDashboard/TradeBooks/TradePriceTable/TradePriceTable.jsx
+++ src/components/organisms/TradeDashboard/TradeBooks/TradePriceTable/TradePriceTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 // css
 import "./TradePriceTable.css";
