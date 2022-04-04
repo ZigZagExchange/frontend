e9b0063e609ca115146c7aa2dd36c468cb8de788
vanndy <uearngold@outlook.com>
2022-04-04 16:26:14 -0400

    added the Text component for the typography

---
+++ src/components/atoms/Text/Text.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import { space, typography as baseTypography, border, color as baseColor } from "styled-system"
+import typography from '../../../lib/theme/typography'
+
+const Text = styled.div`
+  font-family: ${({ font }) => typography[font]?.fontFamily};
+  font-size: ${({ font }) => typography[font]?.fontSize};
+  line-height: ${({ font }) => typography[font]?.lineHeight};
+  color: ${({ color }) => color};
+  letter-spacing: ${({ font }) => typography[font]?.letterSpacing || 'inherit'};
+  text-transform: ${({ font }) => typography[font]?.textTransform || 'inherit'};
+  text-align: ${({ align }) => align || 'left'};
+  word-break: ${({ breakWords }) => breakWords && 'break-word'};
+  ${space}
+  ${baseTypography}
+  ${border}
+  ${baseColor}
+`
+
+export default Text
