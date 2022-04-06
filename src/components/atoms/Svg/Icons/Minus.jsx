3b02a1cbdfc9d6e87d9d4894b4499e02fb700fd8
vanndy <uearngold@outlook.com>
2022-04-06 05:06:40 -0400

    added filled arrow svg icons

--- src/components/atoms/Svg/Icons/Minus.jsx
+++ src/components/atoms/Svg/Icons/Minus.jsx
@@ -7,7 +7,7 @@ const Icon = (props) => {
   const theme = useTheme()
   const { size } = props
   return (
-    <Svg width={size ?? 10} height={size ?? 10} viewBox="0 0 10 10" fill="none" {...props}>
+    <Svg width={size ?? 10} height={size ?? 2} viewBox="0 0 10 2" fill="none" {...props}>
       <path d="M9.33382 1.6668H0.000488281V0.333466H9.33382V1.6668Z" fill={theme.colors.foregroundHighEmphasis}/>
     </Svg>
   )
--- src/components/atoms/Svg/index.jsx
+++ src/components/atoms/Svg/index.jsx
@@ -3,5 +3,9 @@ export { default as MinusIcon } from "./Icons/Minus"
 export { default as CompareArrowIcon } from "./Icons/CompareArrow"
 export { default as EditIcon } from "./Icons/Edit"
 export { default as DeleteIcon } from "./Icons/Delete"
+export { default as DownIcon } from "./Icons/Down"
+export { default as RightIcon } from "./Icons/Right"
+export { default as LeftIcon } from "./Icons/Left"
+export { default as UpIcon } from "./Icons/Up"
 
 export { default as Svg } from "./Svg";
