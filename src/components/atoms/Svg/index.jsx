6819a2e647f74c7481d4ffaa1e089206eb5e3e3c
vanndy <uearngold@outlook.com>
2022-04-06 04:50:01 -0400

    added delete svg icon

--- src/components/atoms/Svg/index.jsx
+++ src/components/atoms/Svg/index.jsx
@@ -2,5 +2,6 @@ export { default as PlusIcon } from "./Icons/Plus"
 export { default as MinusIcon } from "./Icons/Minus"
 export { default as CompareArrowIcon } from "./Icons/CompareArrow"
 export { default as EditIcon } from "./Icons/Edit"
+export { default as DeleteIcon } from "./Icons/Delete"
 
 export { default as Svg } from "./Svg";
