896a32c2fed724953c649249cdfc3547ff5aa3eb
vanndy <uearngold@outlook.com>
2022-04-06 06:34:38 -0400

    Added various svg icons

---
+++ src/components/atoms/Svg/Icons/ExternalLink.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { useTheme } from "styled-components"
+import Svg from "../Svg"
+
+
+const Icon = (props) => {
+  const theme = useTheme()
+  const { size } = props
+  return (
+    <Svg width={size ?? 18} height={size ?? 18} viewBox="0 0 18 18" fill="none" {...props}>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V16H16V9H18V16C18 17.1 17.1 18 16 18H2C0.89 18 0 17.1 0 16V2C0 0.9 0.89 0 2 0H9V2H2Z" fill={theme.colors.foregroundHighEmphasis}/>
+      <path d="M11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z" fill={theme.colors.foregroundHighEmphasis}/>
+    </Svg>
+  )
+}
+
+export default Icon
