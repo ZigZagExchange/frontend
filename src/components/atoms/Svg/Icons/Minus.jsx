04158e5051b6adfddc7d3bd184caeff72485d582
vanndy <uearngold@outlook.com>
2022-04-06 04:39:58 -0400

    added minus svg icon

--- src/components/atoms/Svg/Icons/Minus.jsx
+++ src/components/atoms/Svg/Icons/Minus.jsx
@@ -8,7 +8,7 @@ const Icon = (props) => {
   const { size } = props
   return (
     <Svg width={size ?? 10} height={size ?? 10} viewBox="0 0 10 10" fill="none" {...props}>
-      <path d="M9.33382 5.6668H5.33382V9.6668H4.00049V5.6668H0.000488281V4.33347H4.00049V0.333466H5.33382V4.33347H9.33382V5.6668Z" fill={theme.colors.foregroundHighEmphasis}/>
+      <path d="M9.33382 1.6668H0.000488281V0.333466H9.33382V1.6668Z" fill={theme.colors.foregroundHighEmphasis}/>
     </Svg>
   )
 }
--- src/components/atoms/Svg/index.jsx
+++ src/components/atoms/Svg/index.jsx
@@ -1,3 +1,4 @@
 export { default as PlusIcon } from "./Icons/Plus"
+export { default as MinusIcon } from "./Icons/Minus"
 
 export { default as Svg } from "./Svg";
