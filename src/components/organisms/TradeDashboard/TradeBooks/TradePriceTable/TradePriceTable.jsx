65420eb7bef5490248af761592d76862d481a7ab
TrooperCrypto <trooper.crypto7@gmail.com>
2022-04-09 22:54:27 +0200

    fix and format

--- src/components/organisms/TradeDashboard/TradeBooks/TradePriceTable/TradePriceTable.jsx
+++ src/components/organisms/TradeDashboard/TradeBooks/TradePriceTable/TradePriceTable.jsx
@@ -17,12 +17,15 @@ const TradePriceTable = (props) => {
   };
 
   useEffect(() => {
-    setTimeout(()=>scrollToBottom(), 1000)
+    setTimeout(() => scrollToBottom(), 1000)
   }, [currentMarket]);
 
   let total_total = 0;
-  let total_step = 0;
   props.priceTableData.map((d) => total_total += d.td2);
+  let total_step = (props.className === "trade_table_asks")
+    ? total_total
+    : 0
+
   let onClickRow;
   if (props.onClickRow) onClickRow = props.onClickRow;
   else onClickRow = () => null;
@@ -39,35 +42,37 @@ const TradePriceTable = (props) => {
         </thead>
       )}
       <tbody >
-          {
+        {
           props.priceTableData.map((d, i) => {
-          const color = d.side === "b" ? "#27302F" : "#2C232D";
-          total_step += d.td2; 
-          const breakpoint = Math.round((total_step / total_total) * 100);
-          let rowStyle;
-          if (props.useGradient) {
-            rowStyle = {
-              backgroundImage: `linear-gradient(to left, ${color}, ${color} ${breakpoint}%, #171c28 0%)`,
-            };
-          } else {
-            rowStyle = {};
-          }
-          const price =
-            typeof d.td1 === "number" ? d.td1.toPrecision(6) : d.td1;
-          const amount =
-            typeof d.td2 === "number" ? d.td2.toPrecision(6) : d.td2;
-          const total =
-            typeof d.td3 === "number" ? d.td3.toPrecision(6) : d.td3;
-          return (
-            <tr key={i} style={rowStyle} onClick={() => onClickRow(d)}>
-              <td className={d.side === "b" ? "up_value" : "down_value"}>
-                {price}
-              </td>
-              <td>{numStringToSymbol(amount, 2)}</td>
-              <td>{numStringToSymbol(total, 2)}</td>   
-            </tr>
-          );
-        })}
+            const color = d.side === "b" ? "#27302F" : "#2C232D";
+            total_step = (props.className === "trade_table_asks")
+              ? total_step -= d.td2
+              : total_step += d.td2;
+            const breakpoint = Math.round((total_step / total_total) * 100);
+            let rowStyle;
+            if (props.useGradient) {
+              rowStyle = {
+                backgroundImage: `linear-gradient(to left, ${color}, ${color} ${breakpoint}%, #171c28 0%)`,
+              };
+            } else {
+              rowStyle = {};
+            }
+            const price =
+              typeof d.td1 === "number" ? d.td1.toPrecision(6) : d.td1;
+            const amount =
+              typeof d.td2 === "number" ? d.td2.toPrecision(6) : d.td2;
+            const total =
+              typeof d.td3 === "number" ? d.td3.toPrecision(6) : d.td3;
+            return (
+              <tr key={i} style={rowStyle} onClick={() => onClickRow(d)}>
+                <td className={d.side === "b" ? "up_value" : "down_value"}>
+                  {price}
+                </td>
+                <td>{numStringToSymbol(amount, 2)}</td>
+                <td>{numStringToSymbol(total, 2)}</td>
+              </tr>
+            );
+          })}
       </tbody>
     </table>
   );
