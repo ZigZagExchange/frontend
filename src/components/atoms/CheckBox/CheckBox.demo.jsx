05472fb32fc2550e48ce00a17819e1e3873a3675
vanndy <uearngold@outlook.com>
2022-04-10 03:27:03 -0400

    updated the demo for checkbox

--- src/components/atoms/CheckBox/CheckBox.demo.jsx
+++ src/components/atoms/CheckBox/CheckBox.demo.jsx
@@ -8,21 +8,31 @@ const Row = styled.div`
   grid-gap: 15px;
 `
 
+const Col = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 15px;
+`
+
 const CheckBoxDemo = () => {
     const handleChange = (val) => {
         console.log("ValueChanged: ".concat(val))
     }
   return (
-    <>
+    <Col>
         <Row>
 			<CheckBox Size="Medium" onChange={handleChange}></CheckBox>
 			<CheckBox Size="Medium" onChange={handleChange} isChecked></CheckBox>
-			<CheckBox Size="Medium" onChange={handleChange} labelLeft>Label</CheckBox>
-			<CheckBox Size="Medium" onChange={handleChange} labelLeft isChecked>Label</CheckBox>
-			<CheckBox Size="Medium" onChange={handleChange} labelRight>Label</CheckBox>
-			<CheckBox Size="Medium" onChange={handleChange} labelRight isChecked>Label</CheckBox>
         </Row>
-    </>
+        <Row>
+			<CheckBox Size="Medium" onChange={handleChange} labelLeft>Label Left</CheckBox>
+			<CheckBox Size="Medium" onChange={handleChange} labelLeft isChecked>Label Left</CheckBox>
+        </Row>
+        <Row>
+			<CheckBox Size="Medium" onChange={handleChange} labelRight>Label Right</CheckBox>
+			<CheckBox Size="Medium" onChange={handleChange} labelRight isChecked>Label Right</CheckBox>
+        </Row>
+    </Col>
   );
 };
 
