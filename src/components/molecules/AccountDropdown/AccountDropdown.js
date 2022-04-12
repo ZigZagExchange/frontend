9c30e40cb44f1d9c725272362598c03b5fad24d9
jojo <solux@mail.com>
2022-04-12 13:05:31 +0200

    filter USD balances

--- src/components/molecules/AccountDropdown/AccountDropdown.js
+++ src/components/molecules/AccountDropdown/AccountDropdown.js
@@ -310,9 +310,11 @@ export const AccountDropdown = () => {
 
   const filterSmallBalances = (currency) => {
     const balance = wallet[currency].valueReadable;
+    const usd_balance = coinEstimator(currency) * wallet[currency].valueReadable
+    
     //filter out small balances L2 below 2cents
     if(selectedLayer !== 1){
-      if(balance < 0.02) return false;
+      if(usd_balance < 0.02) return false;
     }
 
     if (balance) {
