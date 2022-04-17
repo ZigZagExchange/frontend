22f3223218d0a022cefd70077992251eec4d622e
vanndy <uearngold@outlook.com>
2022-04-17 14:08:09 -0400

    updated Button component

--- src/components/index.js
+++ src/components/index.js
@@ -20,12 +20,19 @@ export * from "components/atoms/RangeSlider";
 export * from "components/atoms/Dropdown";
 export * from "components/atoms/Modal";
 export * from "components/atoms/Tooltip";
+export * from "components/atoms/InputField";
+export * from "components/atoms/LoadingSpinner";
+export * from "components/atoms/Text";
 
 // Molecules
 export * from "components/molecules/Chart";
 export * from "components/molecules/SpotForm";
 export * from "components/molecules/AccountDropdown";
 export * from "components/molecules/Tabs";
+export * from "components/molecules/TabMenu";
+export * from "components/molecules/Button";
+export * from "components/molecules/Dropdown";
+export * from "components/molecules/Toggle";
 
 // Organisms
 export * from "components/organisms/Header";
