d18ca1055839b920d750ea4647600ce9cb78001f
vanndy <uearngold@outlook.com>
2022-04-17 14:08:39 -0400

    Updated Dropdown component and demo

--- src/components/molecules/Dropdown/Dropdown.demo.jsx
+++ src/components/molecules/Dropdown/Dropdown.demo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components'
 import Dropdown from "./Dropdown";
 import AccountDropdown from "./AccountDropdown";
@@ -11,8 +11,8 @@ const Row = styled.div`
   padding: 10px;
 `
 const data1 = [
-    {text:'zkSync - Mainnet',url:'#'},
-    {text:'zkSync - Rinkeby',url:'#'}
+    {text:'zkSync - Mainnet', value: 1, url:'#'},
+    {text:'zkSync - Rinkeby', value: 1000, url:'#'}
 ]
 const data2 = [
     {text:'DOCS',url:'#', icon: <DocumentIcon />},
@@ -25,18 +25,29 @@ const data3 = [
 ]
 
 const DropdownDemo = () => {
-
-  const clickItem = (text) => {
+  const [context1, setContext1] = useState(data1[0].text)
+  const [context2, setContext2] = useState(data2[0].text)
+  const [context3, setContext3] = useState(data3[0].text)
+  const clickItem1 = (text) => {
+    setContext1(text)
+  }
+  const clickItem2 = (text) => {
+    setContext2(text)
+  }
+  const clickItem3 = (text) => {
+    setContext3(text)
+  }
+  const clickItem4 = (text) => {
     alert(text)
   }
 
   return (
     <>
         <Row>
-            <Dropdown width ={242} item={data1} context="Simple" clickFunction={clickItem}/>
-            <Dropdown width ={242} item={data2} leftIcon context="Left Icon Dropdown" clickFunction={clickItem}/>
-            <Dropdown width ={242} item={data3} rightIcon context="Right Icon Dropdown" clickFunction={clickItem}/>
-            <AccountDropdown width ={242} item={data3} rightIcon clickFunction={clickItem}/>
+            <Dropdown width ={242} item={data1} context={context1} clickFunction={clickItem1}/>
+            <Dropdown width ={242} item={data2} leftIcon context={context2} clickFunction={clickItem2}/>
+            <Dropdown width ={242} item={data3} rightIcon context={context3} clickFunction={clickItem3}/>
+            <AccountDropdown width ={242} item={data3} rightIcon clickFunction={clickItem4}/>
         </Row>
     </>
   );
--- src/components/molecules/Dropdown/Dropdown.jsx
+++ src/components/molecules/Dropdown/Dropdown.jsx
@@ -44,30 +44,31 @@ const IconButton = styled(baseIcon)`
     }
 `
 
-const Dropdown = ({width, item, context, leftIcon, rightIcon, clickFunction}) => {
+const Dropdown = ({width, item, context, leftIcon, rightIcon, transparent, clickFunction}) => {
     const [isOpened, setIsOpened] = useState(false)
 
     const toggle = () => {
         setIsOpened(!isOpened)
     }
 
-    const handleClick = (url, text) => {
+    const handleClick = (url, text, value) => {
         if(url !== '#'){
             window.location.href = url
         }else{
-            clickFunction(text)
+            clickFunction(text, value)
+            toggle()
         }
     }
 
     return (
         <DropdownWrapper> 
-            <ExpandableButton expanded={isOpened} onClick={toggle}>{context}</ExpandableButton>
+            <ExpandableButton transparent={transparent} expanded={isOpened} onClick={toggle}>{context}</ExpandableButton>
             { isOpened &&
                 <Wrapper width={width}>
                         {item.map((items) => {
-                            const {text, url, icon} = items
+                            const {text, value, url, icon} = items
                         return (
-                        <DropdownListContainer key={items.text} leftIcon={leftIcon} onClick={()=> handleClick(url, text)}>
+                        <DropdownListContainer key={items.text} leftIcon={leftIcon} onClick={()=> handleClick(url, text, value)}>
                             {leftIcon && isValidElement(icon) && <IconButton variant="secondary" startIcon={cloneElement(icon)}></IconButton>}
                             <Text font="primaryExtraSmallSemiBold" color="foregroundHighEmphasis">{text}</Text>
 			                {rightIcon && isValidElement(icon) && <IconButton variant="secondary" endIcon={cloneElement(icon)}></IconButton>}
