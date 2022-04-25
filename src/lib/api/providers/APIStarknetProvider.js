ce0d0125ce715dd7b806507294b19d4d2e15d4e5
TrooperCrypto <trooper.crypto7@gmail.com>
2022-04-25 19:21:49 +0200

    fix

--- src/lib/api/providers/APIStarknetProvider.js
+++ src/lib/api/providers/APIStarknetProvider.js
@@ -254,7 +254,7 @@ export default class APIStarknetProvider extends APIProvider {
         try {
           const currencyInfo = this.getCurrencyInfo(currency);
           const minAmountParsed = (minAmount * 10 ** currencyInfo.decimals)
-            .toFixed(currencyInfo.decimals)
+            .toString()
           const newAmountBN = ethers.BigNumber.from(minAmountParsed).mul(25);
           await this._mintBalance(
             currencyInfo.address.toString(),
