273557ae350be1c13929c95d27ea3fd8afcc76f8
woxly <zachary.hunter@sudo.la>
2022-04-29 14:02:17 -0400

    fix: camelCase

--- src/components/molecules/AccountDropdown/AccountDropdown.js
+++ src/components/molecules/AccountDropdown/AccountDropdown.js
@@ -236,11 +236,11 @@ export const AccountDropdown = () => {
 
   const filterSmallBalances = (currency) => {
     const balance = wallet[currency].valueReadable;
-    const usd_balance = coinEstimator(currency) * wallet[currency].valueReadable;
+    const usdBalance = coinEstimator(currency) * wallet[currency].valueReadable;
 
     //filter out small balances L2 below 2cents
     if(selectedLayer !== 1){
-      if(usd_balance < 0.02) return false;
+      if(usdBalance < 0.02) return false;
     }
 
     if (balance) {
