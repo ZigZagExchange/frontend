028308e97e176aa0e274ea34f7f24d7fb6ec06eb
unknown <uearngold@outlook.com>
2022-06-07 09:49:25 +0800

    feat: increased tokendropdownmodal

--- src/components/organisms/TokenDropdownModal/TokenDropDownModal.jsx
+++ src/components/organisms/TokenDropdownModal/TokenDropDownModal.jsx
@@ -37,11 +37,11 @@ const TokenDropDownModal = ({
     query === ""
       ? tickers
       : tickers.filter((token) =>
-          token.name
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+        token.name
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.toLowerCase().replace(/\s+/g, ""))
+      );
 
   return (
     <>
@@ -124,7 +124,7 @@ const TokenDropDownModal = ({
                       >
                         <Combobox.Options
                           open
-                          className=" mt-1 max-h-60 w-full overflow-auto rounded-md dark:bg-[#2B2E4A] bg-sky-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none "
+                          className=" mt-1 max-h-[30rem] w-full overflow-auto rounded-md dark:bg-[#2B2E4A] bg-sky-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none "
                         >
                           {filteredToken.length === 0 && query !== "" ? (
                             <div className="relative px-4 py-2 cursor-default select-none font-work">
@@ -135,8 +135,7 @@ const TokenDropDownModal = ({
                               <Combobox.Option
                                 key={item.id}
                                 className={({ active }) =>
-                                  `relative cursor-default select-none py-2 font-work pl-10 pr-4 mx-3 bg-foreground-100 rounded-md mb-2 border-t border-l border-r border-b dark:border-foreground-400 border-primary-500 ${
-                                    active ? "bg-teal-600" : ""
+                                  `relative cursor-default select-none py-2 font-work pl-10 pr-4 mx-3 bg-foreground-100 rounded-md mb-2 border-t border-l border-r border-b dark:border-foreground-400 border-primary-500 ${active ? "bg-teal-600" : ""
                                   }`
                                 }
                                 value={item}
@@ -144,9 +143,8 @@ const TokenDropDownModal = ({
                                 {({ selected, active }) => (
                                   <>
                                     <span
-                                      className={`flex items-center truncate   ${
-                                        selected ? "font-medium" : "font-normal"
-                                      }`}
+                                      className={`flex items-center truncate   ${selected ? "font-medium" : "font-normal"
+                                        }`}
                                     >
                                       <img
                                         src={api.getCurrencyLogo(item.name)}
@@ -157,11 +155,10 @@ const TokenDropDownModal = ({
                                     </span>
                                     {selected ? (
                                       <span
-                                        className={`absolute  inset-y-0 left-0 flex items-center pl-3 ${
-                                          active
-                                            ? "text-white"
-                                            : "text-teal-600"
-                                        }`}
+                                        className={`absolute  inset-y-0 left-0 flex items-center pl-3 ${active
+                                          ? "text-white"
+                                          : "text-teal-600"
+                                          }`}
                                       >
                                         <CheckIcon
                                           className="w-5 h-5"
