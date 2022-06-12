f22750941b3fd2e3a541a4a591f50ec7b64ceddd
magicdev090 <uearngold@outlook.com>
2022-06-12 07:10:55 -0500

    fixed tab component

--- src/components/molecules/TabMenu/Tab.jsx
+++ src/components/molecules/TabMenu/Tab.jsx
@@ -6,6 +6,14 @@ import Text from "../../atoms/Text/Text";
 const StyledText = styled(Text)`
   width: fit-content;
   padding-bottom: ${({ row }) => row ? '8px' : '16px'};
+  ${({ isActive, theme }) => isActive ? 
+  `background:
+    linear-gradient(93.46deg, ${theme.colors.primaryHighEmphasis} 16.94%, ${theme.colors.secondaryHighEmphasis} 97.24%)
+    left 
+    bottom
+    transparent    
+    no-repeat; 
+  background-size:100% 4px;` : ''}
   color: ${({ left, isActive, theme }) => isActive ? `${theme.colors.primaryHighEmphasis} !important` : !isActive ? `${theme.colors.foregroundMediumEmphasis} !important` : ''};
   transition: color .25s;
 
