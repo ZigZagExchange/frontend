e28b9f3208a0f4299a13d66413323cdccb1c64f6
Trooper <trooper.crypto7@gmail.com>
2022-06-16 00:05:31 +0200

    add axios

--- src/lib/api/providers/APIProvider.js
+++ src/lib/api/providers/APIProvider.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const notImplemented = function (method) {
   const x = () => { throw new Error(`APIProvider method not implemented: ${method}`) }
   x.notImplemented = true
