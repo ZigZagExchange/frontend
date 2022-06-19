fc28c31d9f959b54b673304e344a40fd69f318c8
Trooper <trooper.crypto7@gmail.com>
2022-06-19 23:17:07 +0200

    update msg

--- src/components/molecules/SpotForm/SpotForm.jsx
+++ src/components/molecules/SpotForm/SpotForm.jsx
@@ -329,14 +329,23 @@ export class SpotForm extends React.Component {
       }
     }
 
+    const renderGuidContent = () => {
+      return <div>
+        <p style={{fontSize: '14px', lineHeight:'24px'}}>{this.props.side === 's' ? 'Sell' : 'Buy'} Order pending</p>
+        <p style={{fontSize: '14px', lineHeight:'24px'}}>{baseAmountMsg} {marketInfo.baseAsset.symbol} @ {
+            ['USDC', 'USDT', 'DAI', 'FRAX'].includes(marketInfo.quoteAsset.symbol) ? price.toFixed(2) : formatPrice(price)
+          } {marketInfo.quoteAsset.symbol}</p>
+        <p style={{fontSize: '14px', lineHeight:'24px'}}>Sign or Cancel to continue...</p>
+      </div>
+    }
+
     let newstate = { ...this.state };
     newstate.orderButtonDisabled = true;
     this.setState(newstate);
     const orderPendingToast = toast.info(
-      `Your ${this.props.side === 's' ? 'sell' : 'buy'} Order for ${baseAmountMsg} ${marketInfo.baseAsset.symbol} @ ${
-        ['USDC', 'USDT', 'DAI', 'FRAX'].includes(marketInfo.quoteAsset.symbol) ? price.toFixed(2) : formatPrice(price)} ${
-          marketInfo.quoteAsset.symbol} is pending. Sign or Cancel to continue...`, {
+      renderGuidContent(), {
       toastId: "Order pending",
+      autoClose: false,
       }
     );
 
