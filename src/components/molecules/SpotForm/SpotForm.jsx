dbfcac8c20aa1ed8daf97281494a74508655aca3
hiccupsoft <hiccup1991@hotmail.com>
2022-06-21 05:56:28 -0400

    #503

--- src/components/molecules/SpotForm/SpotForm.jsx
+++ src/components/molecules/SpotForm/SpotForm.jsx
@@ -618,7 +618,7 @@ export class SpotForm extends React.Component {
       )
     } else if (this.props.side === "s") {
       buttonType = "SELL"
-      buttonText = buttonType + " " + (marketInfo && marketInfo.quoteAsset?.symbol);
+      buttonText = buttonType + " " + (marketInfo && marketInfo.baseAsset?.symbol);
       feeAmount = (
         <FormHeader>
           <InfoWrapper>
