ac28216c1ea4993107eb74b293b35f9b29fed2e5
Trooper <trooper.crypto7@gmail.com>
2022-06-29 18:26:05 +0200

    fix toFixed

--- src/lib/api/providers/APIArbitrumProvider.js
+++ src/lib/api/providers/APIArbitrumProvider.js
@@ -108,7 +108,7 @@ export default class APIArbitrumProvider extends APIProvider {
         marketInfo.quoteAsset.decimals
       );
       gasFee = ethers.utils.parseUnits (
-        marketInfo.baseFee.toFixed(marketInfo.baseAsset.decimals),
+        marketInfo.baseFee,
         marketInfo.baseAsset.decimals
       )
     } else {
@@ -123,7 +123,7 @@ export default class APIArbitrumProvider extends APIProvider {
         marketInfo.baseAsset.decimals
       )
       gasFee = ethers.utils.parseUnits (
-        marketInfo.quoteFee.toFixed(marketInfo.quoteAsset.decimals),
+        marketInfo.quoteFee,
         marketInfo.quoteAsset.decimals
       )
     }
