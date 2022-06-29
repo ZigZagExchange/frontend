5866f574ae728fadefdf6839d3998e381fb88925
WonderDev21 <wonder.dev21@gmail.com>
2022-06-30 00:47:45 +0900

    fix: fixed style on fireforx

--- src/components/pages/SwapPage/SwapContainer/SwapContianer.js
+++ src/components/pages/SwapPage/SwapContainer/SwapContianer.js
@@ -52,7 +52,7 @@ const SwapContianer = ({
           Max
         </button>
         <input
-          className="ml-3 text-2xl font-semibold text-right bg-transparent w-36 md:w-auto focus:outline-none"
+          className="ml-3 text-2xl font-semibold text-right bg-transparent w-36 md:w-64 focus:outline-none"
           placeholder="0.00"
           onChange={onChangeFromAmounts}
           value={fromAmounts}
