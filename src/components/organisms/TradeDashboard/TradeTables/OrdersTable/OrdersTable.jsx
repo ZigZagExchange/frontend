ce1edc05f7d97cc1d075eadbcdb12bb8a78624e2
Trooper <trooper.crypto7@gmail.com>
2022-07-01 13:25:11 +0200

    add pm order cancel

--- src/components/organisms/TradeDashboard/TradeTables/OrdersTable/OrdersTable.jsx
+++ src/components/organisms/TradeDashboard/TradeTables/OrdersTable/OrdersTable.jsx
@@ -163,7 +163,7 @@ export class OrdersTable extends React.Component {
                   {statusText}
                 </td>
                 <td data-label="Action">
-                  {["o", "pf"].includes(orderStatus) ? (
+                  {["o", "pf", "pm"].includes(orderStatus) ? (
                     <span
                       className="cancel_order_link"
                       onClick={() => api.cancelOrder(orderId)}
