b266f11bd139cae2cbf2598d9eac731b8af7d8b0
magicdev090 <54931597+MagicDev090@users.noreply.github.com>
2022-07-08 03:04:35 +0900

    disabled resize hanlder on fixing layout mode

--- src/components/organisms/TradeDashboard/TradeDashboard.js
+++ src/components/organisms/TradeDashboard/TradeDashboard.js
@@ -223,6 +223,7 @@ export function TradeDashboard() {
                 }}
                 margin={settings.editable ? [10, 10] : [0, 0]}
                 isDraggable={settings.editable}
+                isResizable={settings.editable}
                 draggableHandle=".grid-item__title"
                 editable={settings.editable}
             >
