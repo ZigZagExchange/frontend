ee1bcc69a8aefc3462ed5eebd2238d3acb4ae767
magicdev090 <uearngold@outlook.com>
2022-07-30 10:02:08 +0900

    changed scroll bar color in trade table

--- src/components/organisms/TradeDashboard/TradeBooks/TradeRecentTable/TradeRecentTable.jsx
+++ src/components/organisms/TradeDashboard/TradeBooks/TradeRecentTable/TradeRecentTable.jsx
@@ -76,13 +76,13 @@ const Table = styled.table`
 
   ::-webkit-scrollbar-track {
     border-radius: 0px;
-    background: hsla(0, 0%, 100%, 0.15) !important;
+    background: transparent;
     height: 23px;
   }
 
   ::-webkit-scrollbar-thumb {
     border-radius: 4px;
-    background: #292d3f21 !important;
+    background: #ffffff21;
   }
 
   ::-webkit-scrollbar-thumb:window-inactive {
