b6cba8dc3ce77d74799821252360e920279ccbf0
magicdev090 <uearngold@outlook.com>
2022-08-02 15:33:58 +0900

    merged master branch

--- src/components/organisms/TradeDashboard/NewFeaturesPopup.jsx
+++ src/components/organisms/TradeDashboard/NewFeaturesPopup.jsx
@@ -18,16 +18,19 @@ const NewFeaturesPopup = () => {
 
   return (
     open && (
-      <div className="max-w-lg p-6 mx-1 border rounded-lg shadow-lg md:mx-0 xl:max-w-sm xl:fixed bottom-5 left-5 dark:bg-background-900 dark:border-foreground-400 border-primary-400 bg-foreground-900  dark:text-foreground-900 text-background-900">
+      <div className="max-w-lg p-6 mx-1 border rounded-lg shadow-lg md:mx-0 xl:max-w-sm fixed bottom-5 left-5 dark:bg-background-900 dark:border-foreground-400 border-primary-400 bg-foreground-900  dark:text-foreground-900 text-background-900">
         <div className="relative">
           <CloseIcon
             className="absolute cursor-pointer right-1 hover:opacity-75"
             onClick={onClose}
           />
-          <p className="text-xl font-semibold font-work">Introducing Dynamic Layouts!</p>
+          <p className="text-xl font-semibold font-work">
+            Introducing Dynamic Layouts!
+          </p>
           <p className="mt-3 text-base ">
-          You can now adjust the interface to your liking by accessing the dynamic layouts feature in the ''Settings'' tab (top right corner).
-          </p>       
+            You can now adjust the interface to your liking by accessing the
+            dynamic layouts feature in the ''Settings'' tab (top right corner).
+          </p>
           <button
             className="float-right mt-6 text-primary-900 hover:underline"
             onClick={onDismissPermanently}
