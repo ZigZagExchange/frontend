c06697e0693fe35a0915f1fd6b66b5c950ec1c7c
taureau <vixidev99@protonmail.com>
2022-08-04 12:22:12 -0700

    disable rinkeby

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -50,13 +50,13 @@ const networkLists = [
     selectedIcon: <CheckIcon />,
     image: zksyncLogo,
   },
-  {
-    text: "zkSync - Rinkeby",
-    value: 1000,
-    url: "#",
-    selectedIcon: <CheckIcon />,
-    image: zksyncLogo,
-  },
+  //{
+  //  text: "zkSync - Rinkeby",
+  //  value: 1000,
+  //  url: "#",
+  //  selectedIcon: <CheckIcon />,
+  //  image: zksyncLogo,
+  //},
   {
     text: "Arbitrum (alpha)",
     value: 42161,
