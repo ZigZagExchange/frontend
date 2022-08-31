02cfc69295abdda4ccba3f4995c5c81a8459f265
WonderDev21 <wonder.dev21@gmail.com>
2022-09-01 01:09:03 +0900

    feat: added flag for dropdown

--- src/components/organisms/Header/LangaugeDropdown/LanguageDropdown.js
+++ src/components/organisms/Header/LangaugeDropdown/LanguageDropdown.js
@@ -9,11 +9,13 @@ const languages = [
     code: "en",
     name: "EN",
     country_code: "gb",
+    icon: "https://raw.githubusercontent.com/OnTheGoSystems/SVG-flags-language-switcher/master/flags/en.svg",
   },
   {
     code: "zh",
-    name: "CHN",
+    name: "中文",
     country_code: "zh",
+    icon: "https://raw.githubusercontent.com/OnTheGoSystems/SVG-flags-language-switcher/master/flags/cn.svg",
   },
 ];
 
@@ -29,7 +31,12 @@ export default function LanguageDropdown() {
     <div className={classNames({ dark: isDark })}>
       <Listbox value={selected} onChange={onChangeLanguage}>
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-8 text-sm font-semibold text-left cursor-pointer hover:text-primary-900 font-work dark:text-white-900 text-foreground400 ">
+          <Listbox.Button className="relative flex items-center w-full gap-2 py-2 pl-3 pr-8 text-sm font-semibold text-left cursor-pointer hover:text-primary-900 font-work dark:text-white-900 text-foreground400 ">
+            <img
+              src={selected.icon}
+              alt={selected.name}
+              className="w-5 rounded-full"
+            />
             <span className="block truncate">{selected.name}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <ChevronDownIcon className="w-5 h-5 mt-1" aria-hidden="true" />
@@ -41,12 +48,12 @@ export default function LanguageDropdown() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute w-26 py-1 mt-1 overflow-auto text-sm dark:bg-[#191A33] border dark:border-foreground-400 bg-sky-100 rounded-md shadow-lg max-h-60">
+            <Listbox.Options className="absolute w-[90px] right-0 py-1 mt-1 overflow-auto text-sm dark:bg-[#191A33] border dark:border-foreground-400 bg-sky-100 rounded-md shadow-lg max-h-60">
               {languages.map((lang, langIdx) => (
                 <Listbox.Option
                   key={langIdx}
                   className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                    `relative cursor-pointer select-none py-2 px-4 ${
                       active
                         ? "dark:bg-[#2B2E4A] bg-[#ecf8fa]"
                         : "text-white-900"
@@ -57,17 +64,22 @@ export default function LanguageDropdown() {
                   {({ selected }) => (
                     <>
                       <span
-                        className={`block truncate ${
+                        className={`flex items-center gap-2 truncate ${
                           selected ? "font-medium" : "font-normal"
                         }`}
                       >
+                        <img
+                          src={lang.icon}
+                          alt={lang.name}
+                          className="w-5 rounded-full"
+                        />
                         {lang.name}
                       </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-primary-900">
+                      {/* {selected ? (
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-primary-900">
                           <CheckIcon className="w-5 h-5" aria-hidden="true" />
                         </span>
-                      ) : null}
+                      ) : null} */}
                     </>
                   )}
                 </Listbox.Option>
