76825b4f5180bdf12adf8dc9d3fbb2184d3c060a
liufulong <307990588@qq.com>
2022-09-04 15:47:53 +0800

    fix: optimize the value of innerHeight

--- src/components/organisms/TradeDashboard/ReactGridLayout/utils.js
+++ src/components/organisms/TradeDashboard/ReactGridLayout/utils.js
@@ -1,7 +1,7 @@
 // Some App webviews are rendered too late and will have an innerHeight of 0
 // The situation needs to be addressed with a compatible solution
 
-const innerHeight =
+export const innerHeight =
   window.innerHeight > 0 ? window.innerHeight : window.screen.height;
 
 export const rowHeight = (innerHeight - 112) / 30;
