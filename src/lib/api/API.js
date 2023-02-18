2e54cae11479d1b78872b3a919f59b4ad004bb58
TrooperCrypto <trooper.crypto7@gmail.com>
2023-02-18 22:37:10 +0100

    revert on network abort

--- src/lib/api/API.js
+++ src/lib/api/API.js
@@ -89,8 +89,8 @@ class API extends Emitter {
     this.rollupProvider = window.ethereum
       ? new ethers.providers.Web3Provider(window.ethereum, "any")
       : new ethers.providers.JsonRpcProvider(
-          `https://${chainName}.infura.io/v3/${this.infuraId}`
-        );
+        `https://${chainName}.infura.io/v3/${this.infuraId}`
+      );
 
     switch (chainName) {
       case "zksync-goerli":
@@ -318,6 +318,8 @@ class API extends Emitter {
             method: "wallet_addEthereumChain",
             params: [ethereumChainInfo],
           });
+        } else {
+          throw switchError
         }
       } catch (addError) {
         console.error(addError);
@@ -346,8 +348,7 @@ class API extends Emitter {
     this.serverDelta = Math.floor((serverTime - clientTime) / 1000);
     if (this.serverDelta < -5 || this.serverDelta > 5) {
       console.warn(
-        `Your PC clock is not synced (delta: ${
-          this.serverDelta / 60
+        `Your PC clock is not synced (delta: ${this.serverDelta / 60
         } min). Please sync it via settings > date/time > sync now`
       );
     }
